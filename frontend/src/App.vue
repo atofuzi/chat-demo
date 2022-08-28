f556c7a5e622120d8042e1a8a7d611619ac0ab9d
atofuzi <58471852+atofuzi@users.noreply.github.com>
2022-08-28 16:07:41 +0900

    make sample(login-from)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,16 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+<div>
+  <div class="bg-gradient-to-tl from-cyan-500 to-blue-500">
+    <div class="max-w-xl mx-auto px-6 text-center h-screen">
+      <h1 class="text-3xl duration-75 ease-in-out text-white">Welcome</h1>
+      <form class="py-4">
+        <input class="block mx-auto my-3 p-2 w-10/12 text-center border border-white/40 bg-white/20 rounded appearance-none placeholder:text-white" type="text" placeholder="Username">
+        <input class="block mx-auto my-3 p-2 w-10/12 text-center border border-white/40 bg-white/20 rounded appearance-none placeholder:text-white" type="password" placeholder="Password">
+        <button class="block mx-auto my-3 p-2 w-10/12 text-center border border-white bg-white rounded" type="submit" id="login-button">Login</button>
+      </form>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -16,12 +25,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
