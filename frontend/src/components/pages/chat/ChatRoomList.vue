39105ea8ef870a75ea2ff1f08ef763c460c773df
atofuzi <58471852+atofuzi@users.noreply.github.com>
2022-11-07 01:50:33 +0900

    チャットルームタイトル表示機能実装

--- frontend/src/components/pages/chat/ChatRoomList.vue
+++ frontend/src/components/pages/chat/ChatRoomList.vue
@@ -1,7 +1,7 @@
 <template>
   <ul class="min-w-[320px] h-[calc(100vh-64px)] overflow-auto">
     <template v-if="chatRoomList.length">
-      <li @click="fetchChatRoom" v-for="chatRoom in chatRoomList" :key="chatRoom.id" :data-chat-room-id="chatRoom.id" class="hover:bg-slate-200 active:bg-slate-300 focus:bg-slate-200">
+      <li @click="fethChatRoom(chatRoom)" v-for="chatRoom in chatRoomList" :key="chatRoom.id" :data-chat-room-id="chatRoom.id" class="hover:bg-slate-200 active:bg-slate-300 focus:bg-slate-200">
           <div class="flex justify-start items-center h-20 mx-3 border-b border-gray-200">
             {{ chatRoom.name }}
           </div>
@@ -12,25 +12,22 @@
 
 <script>
 import { useRouter } from 'vue-router'
-import chatRoom from '@/composables/chatRoom';
 
 // import { useRouter, useRoute } from 'vue-router'
 export default {
-  setup() {
+  props: {
+    chatRoomList: Array
+  },
+  setup(_, {emit}) {
     const router = useRouter();
 
-    const { chatRoomList, chatRoomListError, fetchChatRoomList} = chatRoom();
-
-    fetchChatRoomList();
-
-    const fetchChatRoom = (e) => {
-      const chatRoomId = e.currentTarget.getAttribute('data-chat-room-id');
-      router.push('/chat/' + chatRoomId )
+    const fethChatRoom = (chatRoom) => {
+      // const chatRoomId = e.currentTarget.getAttribute('data-chat-room-id');
+      emit('clickListItem', chatRoom.id);
+      router.push('/chat/' + chatRoom.id );
     }
     return {
-      chatRoomList,
-      chatRoomListError,
-      fetchChatRoom
+      fethChatRoom
     }
   }
 
--- frontend/src/pages/ChatPage.vue
+++ frontend/src/pages/ChatPage.vue
@@ -2,9 +2,9 @@
 <div>
   <HeaderNavigator></HeaderNavigator>
   <div class="flex">
-    <ChatRoomList></ChatRoomList>
+    <ChatRoomList @clickListItem="selectedChatRoom" :chatRoomList="chatRoomList" ></ChatRoomList>
     <main class="flex flex-col h-[calc(100vh-64px)] w-full">
-      <div class="h-12 leading-[48px] text-center border-b bottom-1 shadow">チャットルーム{{ chatRoomId }}</div>
+      <div class="h-12 leading-[48px] text-center border-b bottom-1 shadow">{{ chatRoomName }}</div>
       <div class="overflow-auto px-1 flex-1">
         <ChatMessageList :chatRoomId="chatRoomId"></ChatMessageList>
       </div>
@@ -17,12 +17,13 @@
 </template>
 
 <script>
-import { ref, toRefs, watch } from 'vue';
+import { ref, toRefs, computed } from 'vue';
 // import { useRoute } from 'vue-router'; 
 import HeaderNavigator from '@/components/base/HeaderNavigator.vue';
 import ChatRoomList from '@/components/pages/chat/ChatRoomList.vue';
 import ChatMessageList from '@/components/pages/chat/ChatMessageList.vue';
 import ChatMessageInput from '@/components/pages/chat/ChatMessageInput.vue';
+import chatRoom from '@/composables/chatRoom';
 
 export default {
   name: 'ChatPage',
@@ -36,18 +37,37 @@ export default {
     ChatMessageInput
   },
   setup(props) {
-    // const route = useRoute();
-    /* const chatRoomId = computed( () => { return route.params });  */
 
-    let chatMessage = ref('');
     let { chatRoomId } = toRefs(props);
-    watch(chatRoomId , () => {
-      // console.log(localStorage.getItem('draft_chatmessage_1'));
-      console.log(chatRoomId.value);
+    let selectedChatRoomName = ref('')
+
+    // チャットルーム名取得
+    const getChatRoomName = (chatRoomId) => {
+      // TODO:存在しないchatroom_idの場合のエラー処理
+      const selectedChatRoom = chatRoomList.value.find(({id}) => id === chatRoomId);
+      return selectedChatRoom.name; 
+    } 
+
+    // チャット一覧取得
+    const { chatRoomList, chatRoomListError, fetchChatRoomList} = chatRoom();
+    fetchChatRoomList().then( () => {
+      selectedChatRoomName.value = getChatRoomName(Number(chatRoomId.value)); 
     });
 
+    // チャットルーム選択時の処理
+    const selectedChatRoom = (chatRoomId) => {
+      selectedChatRoomName.value = getChatRoomName(chatRoomId); 
+    }
+
+    const chatRoomName = computed( () => {
+      return selectedChatRoomName.value
+    }) 
+
     return {
-      chatMessage,
+      chatRoomName,
+      chatRoomListError,
+      chatRoomList,
+      selectedChatRoom
     }
   },
 }
